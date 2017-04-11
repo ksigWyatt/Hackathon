c2b2754ed460765a355d3e76b12c58d7833b8d95
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-04-10 20:04:02 -0400

    initial
    Lost all the code…. this is what’s left

---
+++ ObjCBridge.h
@@ -0,0 +1,15 @@
+//
+//  ObjCBridge.h
+//  Hertz Hack
+//
+//  Created by Wyatt Baggett on 4/1/17.
+//  Copyright © 2017 Wyatts Mobile Development. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+
+#endif /* ObjCBridge_h */
